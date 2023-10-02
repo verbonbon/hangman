daea0c077b5f5ce19e9932e1b9edbcc9dfe31b02
Wing Yee Cheung <wingyee.cheung@winchester.ac.uk>
2023-10-02 14:57:44 +0100

    adds blank README markdown file and milestone_3.py

---
+++ milestone_3.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 14:01:27 2023
+
+@author: WingYee.Cheung
+"""
+
+#Write code that will continuously ask the user for a letter and validate it.
+
+#Create a new script called milestone_3.py. This file will contain the code for this milestone.
+
+#Step 1:
+#Create a while loop and set the condition to True. Setting the condition to True` ensures that the code runs continuously.
+
+#In the body of the loop, write the code required for the following steps.
+
+#Step 2:
+#Ask the user to guess a letter and assign this to a variable called guess.
+
+#Step 3:
+#Check that the guess is a single alphabetical character. Hint: You can use Python's isalpha method to check if the guess is alphabetical.
+
+#Step 4:
+#If the guess passes the checks, break out of the loop.
+
+#Step 5:
+#If the guess does not pass the checks, then print a message saying "Invalid letter. Please, enter a single alphabetical character."
